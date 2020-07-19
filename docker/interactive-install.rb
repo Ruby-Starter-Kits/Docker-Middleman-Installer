626eaec012794f15a4036f75c610151696a23c14
Mike Rogers <me@mikerogers.io>
2020-07-19 16:51:04 +0100

    Using rsync to copy hidden files

--- docker/interactive-install.rb
+++ docker/interactive-install.rb
@@ -10,8 +10,7 @@ require "readline"
 system("middleman init #{@app_path} #{@arguments}")
 
 # Copy the docker files
-system("shopt -s dotglob")
-system("cp -ra /usr/src/App-Template/* /usr/src/app/#{@app_path}/")
+system("rsync -a /usr/src/App-Template/ /usr/src/app/#{@app_path}/")
 
 # Copy sample files
 system("cp -ra /usr/src/App-Template/.env.sample /usr/src/app/#{@app_path}/.env")
